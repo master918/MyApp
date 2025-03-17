7e43320c67ffbfbec4854bfb39e99a8bfe76a17d
ПО Александр <poteshkin.sashuta@gmail.com>
2025-03-17 11:23:39 +0300

    Добавьте файлы проекта.

---
+++ MyApp/MyApp/AppShell.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using MyApp.ViewModels;
+using MyApp.Views;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MyApp
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            // Сброс состояния входа
+            Preferences.Set("IsLoggedIn", false);
+
+            // Переход на страницу входа
+            await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+
+            //await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ MyApp/MyApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,60 @@
+﻿using MyApp.Views;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MyApp.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+        private string username;
+        private string password;
+
+        public string Username
+        {
+            get => username;
+            set
+            {
+                username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+
+        public string Password
+        {
+            get => password;
+            set
+            {
+                password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+        }
+
+        private async void OnLoginClicked(object obj)
+        {        
+            // Пример проверки логина и пароля
+            if (username == "" && password == "") //
+            {
+                // Сохранение состояния входа
+                Preferences.Set("IsLoggedIn", true);
+                Username = "";
+                Password = "";
+                await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+            }
+            else 
+            {
+                await Application.Current.MainPage.DisplayAlert("Ошибка", "Неверный логин или пароль!", "OK");
+            }
+        }
+    }
+}
