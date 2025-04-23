247f2f94c2d524488be9c6eb45355c9ec17edc00
master918 <poteshkin.sashuta@gmail.com>
2025-04-23 20:36:35 +0300

    GoogleSheet

--- MyApp/MyApp/App.xaml.cs
+++ MyApp/MyApp/App.xaml.cs
@@ -4,15 +4,21 @@ using Xamarin.Forms.Xaml;
 using MyApp.Services;
 using MyApp.Views;
 using Xamarin.Essentials;
+using MyApp.Models;
+using System.Collections.ObjectModel;
 
 namespace MyApp
 {
     public partial class App : Application
     {
+        public ObservableCollection<Item> Items { get; } = new ObservableCollection<Item>();
 
-        public App ()
+        public App()
         {
             InitializeComponent();
+
+            LoadDataAsync();
+
 
             DependencyService.Register<MockDataStore>();
             MainPage = new AppShell();
@@ -29,16 +35,39 @@ namespace MyApp
             }
         }
 
-        protected override void OnStart ()
+        private async void LoadDataAsync()
         {
+            GoogleService service = new GoogleService();
+            var sheetData = await service.GetSheetDataAsync(); // Ожидание завершения асинхронного операции
 
+            foreach (var row in sheetData) // Теперь sheetData - это IList<IList<object>>
+            {
+                Items.Add(new Item
+                {
+                    Id = row[0]?.ToString() ?? string.Empty,
+                    Text = row[1]?.ToString() ?? string.Empty,
+                });
+            }
         }
 
-        protected override void OnSleep ()
+        protected override async void OnStart()
+        {
+            // Проверяем оба типа авторизации
+            if (Preferences.Get("IsLoggedIn", false))
+            {
+                MainPage = new AppShell();
+            }
+            else
+            {
+                MainPage = new LoginPage();
+            }
+        }
+
+        protected override void OnSleep()
         {
         }
 
-        protected override void OnResume ()
+        protected override void OnResume()
         {
         }
     }
